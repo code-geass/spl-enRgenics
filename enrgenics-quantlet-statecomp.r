9d426477063ee2ea795f1ed47232a9731d658755
David Pollack <sent@da3.net>
2016-05-29 12:21:32 +0000

    init quantlet 2 - state comparisons

---
+++ enrgenics-quantlet-statecomp.r
@@ -0,0 +1,53 @@
+
+# load enrgenics-quantlet-import
+source("enrgenics-quantlet-import.r")
+
+
+file = "data/sales_revenue_1.csv"
+df = load_eia_data(file)
+
+head(df)
+
+totals = subset(df, DataStatus=="Final")
+# tmp = dcast(totals[which(totals$Cat == "TOTAL"),], Year + Month ~ State + Cat, value.var="Sales", fun.aggregate = sum, na.rm = TRUE)
+ts_year_by_state = dcast(totals[which(totals$Cat == "TOTAL"),], Year ~ State, value.var="Sales", fun.aggregate = sum)
+corr_states = cor(ts_year_by_state[, c(-1,-ncol(ts_year_by_state))])
+
+
+heatmap(corr_states, Rowv=NA, Colv=NA, col = cm.colors(256), margins=c(5,10))
+
+# consider weather data
+# https://ram-n.github.io/weatherData/
+
+# mapping without advanced libs
+# http://stackoverflow.com/questions/24441775/how-do-you-create-a-us-states-heatmap-based-on-some-values
+
+library(MASS)
+library(maps)
+library(maptools)
+library(sp)
+
+permit = gpclibPermit()
+
+txt <- "AB  AK  AL  AN  AR  AZ  CA  CO  CT  DC  DE  EN  FL  GA  HI  IA  ID  IL  IN  KS
+    1  21  31   1  12  56 316  53  31  16   7   1 335  63  11  42  29  73  40  2"
+
+dat <- stack(read.table(text = txt,  header = TRUE))
+names(dat)[2] <-'state.abb'
+dat$states <- tolower(state.name[match(dat$state.abb,  state.abb)])
+
+mapUSA <- map('state',  fill = TRUE,  plot = FALSE)
+nms <- sapply(strsplit(mapUSA$names,  ':'),  function(x){x[1]})
+USApolygons <- map2SpatialPolygons(mapUSA,  IDs = nms,  CRS('+proj=longlat'))
+    
+idx <- match(unique(nms),  dat$states)
+dat2 <- data.frame(value = dat$value[idx], state = unique(nms))
+row.names(dat2) <- unique(nms)
+
+USAsp <- SpatialPolygonsDataFrame(USApolygons,  data = dat2)
+    
+spplot(USAsp['value'])
+
+dat2
+# https://www.census.gov/popest/data/datasets.html
+# https://docs.google.com/spreadsheets/d/1JsV5bNnFoE-4xapsnrpqRP-5wgTkpDjgC0SXKW8zgPI/edit#gid=0
--- notes.md
+++ notes.md
@@ -4,16 +4,18 @@
 2. chmod miniconda sh file and then run, after running delete installer
 2. export miniconda to path (export PATH="/home/ubuntu/miniconda3/bin:$PATH")
 3. conda install jupyter matplotlib
-4. conda install -c r r-essentials
+4. conda install -c r r-essentials r-reshape2 r-zoo r-maptools
 4. conda clean --tarballs
 5. end up using about 1 gig of space
 
 
 ## jupyter URL and commands
 
-https://spl-enrgenics-dhpollack.c9users.io/
+https://spl-enrgenics2-dhpollack.c9users.io/
 
 jupyter notebook --no-browser --port $PORT --ip $IP
+
+[auto save script](http://protips.maxmasnick.com/ipython-notebooks-automatically-export-py-and-html)
 
 ## noteworthy links
 
