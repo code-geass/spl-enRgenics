493b7b8616e9ac1b4fb6e6b90429a5e1afdab6be
David Pollack <sent@da3.net>
2016-06-10 13:17:28 +0000

    more graphing tests

--- enrgenics-quantlet-statecomp.r
+++ enrgenics-quantlet-statecomp.r
@@ -14,9 +14,6 @@ ts_year_by_state = dcast(totals[which(totals$Cat == "TOTAL"),], Year ~ State, va
 corr_states = cor(ts_year_by_state[, c(-1,-ncol(ts_year_by_state))])
 means_by_state = colMeans(ts_year_by_state[,-1])
 
-corr_states
-
-
 heatmap(corr_states, Rowv=NA, Colv=NA, col = cm.colors(256), margins=c(5,10))
 
 #http://www.r-bloggers.com/ggplot2-cheatsheet-for-scatterplots/
@@ -24,17 +21,26 @@ library(ggplot2)
 # plot options for Jupyter Notebook
 options(repr.plot.width = 12, repr.plot.height = 5)
 
-totals = subset(df, DataStatus=="Final")
-totals_sub = subset(totals, State=="DE" & Cat == "TOTAL" & Year < 2000)
+# Plot Set 1
+totals = subset(df, DataStatus=="Final" & Cat == "TOTAL")
+totals$DateFormat = as.Date(as.POSIXct(totals$Date, origin="1970-01-01"))
+totals_sub = subset(totals, State=="DE")
 totals_sub$DateFormat = as.Date(as.POSIXct(totals_sub$Date, origin="1970-01-01"))
-p1 <- ggplot(totals_sub, aes(x = DateFormat, y = Revenue))
+p1 = ggplot(totals_sub, aes(x = DateFormat, y = Revenue))
 p1 + geom_point(aes(color=factor(Year)))
-ggsave("output/plt1.png", width = 16, height = 9, dpi = 120)
+ggsave("output/plt1a.png", width = 16, height = 9, dpi = 120)
 p1 + geom_point(aes(color=factor(Month)))
-ggsave("output/plt2.png", width = 16, height = 9, dpi = 120)
-head(totals_sub)
+ggsave("output/plt1b.png", width = 16, height = 9, dpi = 120)
+p1c = ggplot(totals, aes(x = DateFormat, y = Revenue))
+p1c + geom_point(aes(color=factor(State)))
+ggsave("output/plt1c.png", width = 30, height = 20, dpi = 120)
 
 
+# Plot Set 2
+forp2 = ts_year_by_state
+forp2$total = apply(ts_year_by_state[,2:ncol(ts_year_by_state)], 1,function(x){sum(x)})
+p2 = ggplot(forp2, aes(x = Year, y = total))
+p2 + geom_point(aes(color=factor(Year)))
 
 # consider weather data
 # https://ram-n.github.io/weatherData/
